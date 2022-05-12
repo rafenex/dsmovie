8c095ea1a7d4b216c3b4ce7a988b2224afbdcf57
Rafael <dvr.rafa@gmail.com>
2022-05-11 23:50:53 -0300

    Alterando arquivo

--- frontend/src/Components/navbar/index.tsx
+++ frontend/src/Components/navbar/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 
@@ -8,7 +8,7 @@ function Navbar() {
       <nav className="container">
         <div className="dsmovie-nav-content">
           <h1>DSMovie</h1>
-          <a href="https://github.com/rafenex" target="_blank" rel="noreferrer">
+          <a href="https://github.com/rafenex">
             <div className="dsmovie-contact-container">
               <GithubIcon />
               <p className="dsmovie-contact-link">/rafenex</p>
