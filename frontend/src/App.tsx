2da1f7754e2e431716aaf008e2765491d89a194c
Rafael <dvr.rafa@gmail.com>
2022-05-12 07:31:34 -0300

    ERRO NO BUILD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,12 +5,12 @@ import {
 } from "react-router-dom";
 import Listing from 'pages/Listing';
 import Form from 'pages/Form';
-import Navbar from "components/Navbar";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
+
       <Routes>
         <Route path="/" element={<Listing />} />
         <Route path="/form">
